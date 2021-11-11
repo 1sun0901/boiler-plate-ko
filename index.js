4bd780987ab8f258186360affbcf73dac29757c1
임원선(Wonsun Lim) 차장 (주)두산 디지털이노베이션 <wonsun.lim@doosan.com>
2021-11-11 17:31:35 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+const config = require('./config/key');
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://emuce:Dlantm3837!@cluster0.nuvgp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     // useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected.dasdfsafdd..'))
+}).then(() => console.log('MongoDB Connected...새해복..'))
   .catch(err => console.log(err))
-
-
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+
+  console.log('11111111111.....');
+  const user = new User(req.body)
+
+  user.save((err,userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
